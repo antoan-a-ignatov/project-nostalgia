3a04d3fc41be3ace3ce1ea5e90435d2c069b5a70
antoan-a-ignatov <antoan.a.ignatov@gmail.com>
2026-05-25 11:37:27 +0000

    Start of Sidebar work

--- client/src/layout/AppSidebar.tsx
+++ client/src/layout/AppSidebar.tsx
@@ -285,7 +285,7 @@ const AppSidebar: React.FC = () => {
 
   return (
     <aside
-      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
+      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-gray-300 text-black h-screen transition-all duration-300 ease-in-out z-50 border-t-2 border-l-2 border-white border-r-2 border-b-2 border-r-gray-700 border-b-gray-700
         ${
           isExpanded || isMobileOpen
             ? "w-[290px]"
